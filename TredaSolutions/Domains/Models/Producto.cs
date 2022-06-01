35d17613c9555f60b27e54bf8b83613a8d7dca97
jose moncada <andresmonk2010@hotmail.com>
2022-06-01 09:06:18 -0500

    Agregar archivos de proyecto.

---
+++ TredaSolutions/Domains/Models/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TredaSolutions.Domains.Models
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int SKU { get; set; }
+        [Required]
+        public string Descripcion { get; set; }
+        [Required]
+        public int Valor { get; set; }
+        [Required]
+        public int Tienda { get; set; }
+
+    }
+}
---
+++ TredaSolutions/Domains/Models/Tienda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TredaSolutions.Domains.Models
+{
+    public class Tienda
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Column(TypeName ="varchar(50)")]
+        public string Nombre { get; set; }
+        public DateTime FechaApertura { get; set; }
+    }
+}
