5aea95a58146eb3c8a5c140c9177e01182ea8ea9
jose moncada <andresmonk2010@hotmail.com>
2022-06-01 09:31:20 -0500

    Se crea la conexion hacia la base datos

---
+++ TredaSolutions/Persitence/Context/AplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TredaSolutions.Persitence.Context
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+        }
+    }
+}
